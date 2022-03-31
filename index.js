3e7e97557842b154968bd9b1bcdf7356ba1b85f9
stuart wilson <stuartwilson94@gmail.com>
2022-03-31 17:17:12 -0500

    added modules that allow for admin to add products to the website

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const authRouter = require('./routes/admin/auth');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    cookieSession({
-    keys: ['asljkdfowiejjsdiuigfs932jla']
-}));
+	cookieSession({
+		keys: ["asljkdfowiejjsdiuigfs932jla"],
+	})
+);
 
 app.use(authRouter);
+app.use(productsRouter);
+
+app.use(express.static("public"));
 
 app.listen(3000, () => {
-    console.log('listening');
+	console.log("listening");
 });
